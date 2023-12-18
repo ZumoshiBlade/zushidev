c2b9c8cc6404fcdf454cf731bc43e41213fcc6df
ZumoshiBlade <jorgetapia0528@gmail.com>
2023-12-18 15:58:04 -0300

    vImagenes

--- zushidev/urls.py
+++ zushidev/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
+from django.views.static import serve
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -24,6 +25,11 @@ urlpatterns = [
     path('', include("core.urls"))
 ]
 
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root' : settings.MEDIA_ROOT,
+    })
+]
 
 # Ingresa a el media url solo cuando estemos en modo DESARROLLO
 if settings.DEBUG:
